66882f2ae3c0615bbb2aafa9b4088dad329f5a0e
kskaneki1 <kskaneki5@gmail.com>
2024-02-08 23:47:36 +0800

    Rainbow!

---
+++ porfolio/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import {inspect} from "util";
+import styles from './styles.module.css';
+
+
+export default function Home() {
+  return (
+      <main className="flex min-y-screen items-center justify-start p-10">
+          <div className={styles.animatedContainer}>
+              <h1 className={styles.text}>
+                  Who Am I?
+              </h1>
+              <p className="text-gray-600">
+                  "Who am I?" is a question with no definite answer.
+              </p>
+          </div>
+      </main>
+  );
+}
